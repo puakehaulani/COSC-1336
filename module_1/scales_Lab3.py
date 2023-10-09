11494a8706bfd82de579a5bc4c1de1577f1181fc
puakehaulani <puakehaulani@gmail.com>
2023-10-08 20:44:11 -0500

    week3 lab cleanup

--- module_1/scales_Lab3.py
+++ module_1/scales_Lab3.py
@@ -1,30 +1,6 @@
-# Lab 3 Decision Structures
-
-# Problem 1.  Volume Discounts
-# Geekware Software Company’s biggest seller software package sells for $149 each.  Quantity discounts are given according to the following table:
-# Quantity 
-# Discount
-# 10-49
-# 10%
-# 50-99
-# 20%
-# 100-149
-# 30%
-# 150 or more
-# 40%
-
-# Write a program that asks the user to enter the number of packages purchased.  The program should then display the dollar amount of the discount (if any) and the total amount of the purchase after discount.
-# See Section 3.4 for information and examples of nested decision structures and multiple nested decision structures.
-# Code.  Write your program in Python using the above steps. Save your program as a .py file with the name yourlastname_Lab3.py 
-# Output.  Your program should produce correctly labeled output with dollar amounts rounded to 2 decimal places and dollar signs displayed.  See section 2.8 More About Output.  Sample dialog:
-# Enter the number of packages purchased: 53
-# Discount Amount: $ 1,579.40
-# Total Amount: $ 6,317.60
-
-
-# lexi scales, program status
-# This program calculates bulk discount amounts for Geekware Software Company packages.
-# The user enters the number of packages they purchased, and the program will calculate then display the dollar amount of the discount, if any, and the total cost of the order after discount.
+# lexi scales, program complete
+# this program calculates bulk discount amounts for Geekware Software Company packages.
+# the user enters the number of packages they purchased, and the program will calculate then display the dollar amount of the discount, if any, and the total cost of the order after discount.
 
 BASE_PRICE = 149
 number_purchased = int(input('Enter the number of packages purchased: '))
