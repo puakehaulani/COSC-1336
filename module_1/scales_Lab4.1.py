5da88589ca7d90074cef2799bb63f27adaf07840
puakehaulani <puakehaulani@gmail.com>
2023-10-11 20:42:23 -0500

    week4 lab 1 complete

--- module_1/scales_Lab4.1.py
+++ module_1/scales_Lab4.1.py
@@ -1,24 +1,21 @@
-# Chapter 4 Loops
-# Problem 1.  Budgeted vs. Actual Expenses
-# Write program that asks the user to enter the amount that he or she has budgeted for a month.  Using a while loop, the user should be prompted to enter each of his or her expenses for the month and keep a running total.  When the loop finishes, the program should display the amount that the user is over or under budget.
-# Design. Use an IPO diagram to design your program. See IPO diagram attached to this assignment.   
-# Code.  Write your program in Python using the above steps. Save your program as a .py file with the name yourlastname_Lab4.1.py 
-# Output.  Your program should produce correctly labeled output with dollar amounts rounded to 2 decimal places and dollar signs.  Sample dialog:  
-# Enter amount budgeted for the month: 500
-# Enter an amount spent(0 to quit): 50
-# Enter an amount spent(0 to quit): 75
-# Enter an amount spent(0 to quit): 800
-# Enter an amount spent(0 to quit): 23.95
-# Enter an amount spent(0 to quit): 0
-# Budgeted: $ 500.00
-# Spent: $ 948.95
-# You are $ 448.95 over budget. PLAN BETTER NEXT TIME!
+# lexi scales, program complete
+# This is a budgeting program. The user enters their monthly budget, and then is prompted to enter expenses. The program totals their expenses, and tells them if they were over or under budget, and by how much.
 
-
-# Programming Style Requirements.  
-# Comments – Begin your program with a comment that includes: a) your name, b)program status – either “Complete” or describe any incomplete or non-functioning part of your program c)A 1-3 line description of what the program does.
-# Variable names – use meaningful variable names such as total_taxes or num_cookies.
-# Named constants – Use named constants for all number values that will not be changed in the program such as RECIPE_SUGAR = 1.5.   See section 2.9 on Named Constants
-
-# You will have two .py  files and one IPO file to turn in to Blackboard.  
+print('Budgeting Program')
+total_expenses = 0
+budget = float(input('Enter amount budgeted for the month: '))
+while budget < 0:
+    print('Error: Invalid entry. Enter a positive number.')
+    budget =float(input('Enter amount budgeted for the month: '))
+expense = float(input('Enter an amount spent(0 to quit): '))
+while expense != 0:
+    total_expenses += expense
+    expense = float(input('Enter an amount spent(0 to quit): '))
+difference = budget - total_expenses
+print(f'Budgeted: ${budget:,.2f}')
+print(f'Spent: ${total_expenses:,.2f}')
+if difference < 0:
+    print(f'You are ${abs(difference):,.2f} over budget (ㅠ﹏ㅠ)')
+else:
+     print(f'You are ${difference:,.2f} under budget (੭˃ᴗ˂)੭')
 
