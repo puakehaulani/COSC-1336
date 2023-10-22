5d8339d64614545a466474d841dfd765865f8d4d
puakehaulani <puakehaulani@gmail.com>
2023-10-21 20:37:53 -0500

    completed second practice and tested

--- module_2/scales_Practice5A.2.py
+++ module_2/scales_Practice5A.2.py
@@ -10,3 +10,24 @@
 # user
 # 5. Use a global constant for the conversion factor
 # 6. Turn in your program to the practice assignment link in course content.
+
+# This program converts kilometers to miles
+CONVERSION_RATE = 0.6214
+def main():
+    start_message()
+    kilometers = float(input('Enter number of km: '))
+    conversion(kilometers)
+    goodbye_message()
+
+
+def start_message():
+    print('This program converts kilograms to miles')
+
+
+def conversion(kilometers):
+    print(f'\nThere are {kilometers * CONVERSION_RATE:,.2f} mi in {kilometers} km\n')
+    
+def goodbye_message():
+    print('Hope that helped!')
+    
+main()
