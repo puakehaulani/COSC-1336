eaac074ab4e6ab667d0967fe1658b9731c76b50d
puakehaulani <puakehaulani@gmail.com>
2023-10-23 19:09:15 -0500

    added instructions for week5b

---
+++ module_2/scales_Practice5B.1.py
@@ -0,0 +1,5 @@
+# In Python, write a program to generate a random floating point number between 1
+# and 100 which will be used as the radius of a circle. In a separate function, calculate
+# the area of a circle based on the radius your program just generated and return it to
+# the main function. In the main function, display the radius and the area.
+# Turn in your program to the practice assignment link in course content.
---
+++ module_2/scales_Practice5B.2.py
@@ -0,0 +1,16 @@
+# Work in groups of two (online students work on your own). You will
+# write a program that will calculate the area of a square based on a
+# randomly generated integer to be used as the side.
+# Person 1 –write the main function which will:
+# 1. Generate a random integer between 1 and 100 which will be the
+# side of the square
+# 2. Pass that number to called functions called square_area and
+# square_perimeter.
+# 3. Display the side, the returned area, and the returned perimeter.
+# Person 2 – in a separate file called square.py:
+# 1. Define a function called square_area which will accept the side
+# from main, calculate the square area, and returns it to main.
+# 2. Define a function called square_perimeter which will accept
+# the side from main, calculate the square perimeter, and return it
+# to main.
+# Turn in your 2 .py files to the practice assignment link in course content.
