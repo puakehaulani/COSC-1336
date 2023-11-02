0bb76dccac5293bc928fb9bf347932ffbe38744a
puakehaulani <puakehaulani@gmail.com>
2023-11-02 18:59:00 -0500

    finished 6b2

--- module_2/ch6/scales_Practice6B.2.py
+++ module_2/ch6/scales_Practice6B.2.py
@@ -9,3 +9,45 @@
 # converted to numbers
 # c. Handle any other unspecified errors
 # Turn in your program to the practice assignment link in course content.
+
+
+# this program reads a file of numbers, calculates the sum and average of the numbers, and displays these amounts.
+def main():
+    pass
+    # initialize counter var
+    count = 0
+    # initialize sum var
+    sum = 0
+    # try suite
+    try: 
+    # open file
+        infile = open('number_list.txt', 'r')
+        # for loop to go through each line
+        for line in infile:
+            # add one to the counter var
+            count += 1
+            # convert the number to an int and add number to the sum var
+            num = int(line)
+            sum += num
+    # IOError exceptions
+    except IOError as err:
+        print(f'\tERROR: Problem reading file\n\t{err}')
+    # ValueError exceptions
+    except ValueError as err:
+        print(f'\tERROR: Record is not a valid number\n\t{err}')
+    # unspecified errors
+    except Exception as err:
+        print(f'\tERROR: An error has occurred\n\t{err}')
+    # close file
+    infile.close()
+    
+    print('╭── ⋅ ⋅ ── ⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ── ⋅ ⋅ ──╮')
+     # print the sum
+    print(f'the sum of the valid numbers in this file is {sum:,}')
+    # print the average (sum/count)
+    average = sum/count 
+    print(f'the average of the valid numbers in this file is {average:,.2f}')
+    print('╰── ⋅ ⋅ ── ⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ── ⋅ ⋅ ──╯')
+    
+if __name__ == '__main__':
+    main()
