dfac0d1512122f9f3465e7532df9972e8921b4a6
puakehaulani <puakehaulani@gmail.com>
2023-11-02 15:16:50 -0500

    practice 6a complete

--- module_2/ch6/scales_Practice6A2.1.py
+++ module_2/ch6/scales_Practice6A2.1.py
@@ -5,3 +5,25 @@
 # (the second method we discussed).
 # Turn in your two program files to the practice assignment link
 # in course content.
+
+# this program reads numbers from a file and displays them
+
+def main():
+    # open file
+    infile = open('number_list.txt', 'r')
+    # read first line
+    line = infile.readline()
+    # while there are lines in the file, read the contents
+    while line != '':
+        # convert line to integer
+        num = int(line)
+        # print line
+        print(f'{num}')
+        # read next line
+        line = infile.readline()
+    # close file
+    infile.close()
+    
+
+if __name__ == '__main__':
+    main()
