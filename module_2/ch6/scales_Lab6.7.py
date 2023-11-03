58019cede5f185d5e41aa35e9098b663ae56e0f1
puakehaulani <puakehaulani@gmail.com>
2023-11-02 20:00:29 -0500

    cleanup

--- module_2/ch6/scales_Lab6.7.py
+++ module_2/ch6/scales_Lab6.7.py
@@ -1,21 +1,3 @@
-# Lab 6 – Files:  Random Numbers 
-
-# You will create each program in the IDLE environment and save it as a python file.  Use the file naming convention yourlastname_Lab6.n.py  where “n” is the exercise number.
-
-# 7 Random Number File Writer
-# Write a program that writes a series of random numbers to a file. Each random number should be in the range of 1 through 500. The application should let the user specify how many random numbers the file will hold.
-
-
-# Programming Style Requirements.  
-# 	•	Comments – Begin your program with a comment that includes: a) your name, b)program status – either “Complete” or describe any incomplete or non-functioning part of your program c)A 1-3 line description of what the program does.
-# 	•	Function comments – each function should begin with a comment explaining what the function does
-# 	•	Variable names – use meaningful variable names such as total_taxes or num_cookies.
-# 	•	Function names – use meaningful verb names for functions such as display_taxes. 
-# 	•	Named constants – Use named constants for all number values that will not be changed in the program such as RECIPE_SUGAR = 1.5.   See section 2.9 on Named Constants
-
-# You should have 2 program files to turn in.
-
-
 # lexi scales. complete.
 # this program writes a series of random numbers to a file. each random number is between 1 and 500. the user will determine how many random numbers the file should have. 
 
--- module_2/ch6/scales_Lab6.8.py
+++ module_2/ch6/scales_Lab6.8.py
@@ -1,21 +1,4 @@
-# 8 Random Number File Reader
-# This exercise assumes you have completed Programming Exercise 7. Write another program that reads the random numbers from the file, displays the numbers, then displays the following data:
-#   The total of the numbers
-#   The number of random numbers read from the file
-#   The average of the random numbers
-
-# Exception Handling:   Both programs should use Try and Except Clauses to handle IOError, ValueError, and unspecified error exceptions.   Be sure to test your programs for invalid cases.  For example ,  try running the second program to read the file when it doesn’t exist or manually put bad data in the file and see what happens.  
-
-# Programming Style Requirements.  
-# 	•	Comments – Begin your program with a comment that includes: a) your name, b)program status – either “Complete” or describe any incomplete or non-functioning part of your program c)A 1-3 line description of what the program does.
-# 	•	Function comments – each function should begin with a comment explaining what the function does
-# 	•	Variable names – use meaningful variable names such as total_taxes or num_cookies.
-# 	•	Function names – use meaningful verb names for functions such as display_taxes. 
-# 	•	Named constants – Use named constants for all number values that will not be changed in the program such as RECIPE_SUGAR = 1.5.   See section 2.9 on Named Constants
-
-# You should have 2 program files to turn in.
-
-# lexi scales. incomplete.
+# lexi scales. complete.
 # this program reads a file of numbers then displays the numbers, their sum, and the amount of numbers read from the file.
 
 # this is the main function of the program. it contains the program logic. 
