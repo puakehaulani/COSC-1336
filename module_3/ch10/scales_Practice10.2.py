a81af770af02b5960eecedf4f9484f79c47f6b3a
puakehaulani <puakehaulani@gmail.com>
2023-11-08 10:15:52 -0600

    ch10 assignment instructions

---
+++ module_3/ch10/scales_Practice10.2.py
@@ -0,0 +1,30 @@
+# Practice Assignment Ch. 10-2
+# Work in groups of 2. (Online students work on your own) One person
+# writes the class, one writes the program to implement the class.
+# You should have 2 files:
+# • One for the class
+# • One for the class implementation (main program)
+
+# 1. Pet Class
+# Write a class named Pet, which should have the following data attributes:
+# • __name (for the name of a pet)
+# • __animal_type (for the type of animal that a pet is. Example values are 'Dog', 'Cat', and 'Bird')
+# • __age (for the pet's age)
+
+# The Pet class should have an __init__ method that creates these atrributes. It should also have the following methods:
+
+# • set_name
+#   This method assigns a value to the __name field
+# • set_animal_type
+#   This method assigns a value to the __animal_type field
+# • set_age
+#   This method assigns a value to the __age field
+# • get_name
+#   This method returns the value of the __name field
+# • get_animal_type
+#   This method returns the value of the __animal_type field
+# • get_age
+#   This method returns the value of the __age field
+
+# Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet.
+# This data should be stored as the object's attributes. Use the object's accessor methods to retrieve the pet's name, type, and age, and display this data on the screen.
