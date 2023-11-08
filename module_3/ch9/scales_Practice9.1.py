db0d4d68e58dbc09000f5ebbd668cdfefa5e385f
puakehaulani <puakehaulani@gmail.com>
2023-11-08 10:15:43 -0600

    ch9 assignment instructions

---
+++ module_3/ch9/scales_Practice9.1.py
@@ -0,0 +1,13 @@
+# Practice Assignment Ch. 9-1
+# 1. Classroom Sections: Group assignment – break into groups of 3 or 4.
+# 2. Online students: do on your own.
+# 3. Using the example of the birthday dictionary in Chapter 9, create a dictionary application, subject of your choosing
+# (appropriate content)
+# 4. In addition to the menu choices provided to the user in the example, add menu items to display the entire
+# dictionary.
+# 5. Each person will create a separate module for their function(s) with the main program importing all the modules.
+#  Person 1 – main program/menu display, coordinates other programs
+#  Person 2 – Look up and display functions
+#  Person 3 – Add function
+#  Person 4 - Change, and Delete functions
+# Turn in your programs/modules to the practice assignment link in course content.
