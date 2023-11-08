5a802375283a52f3e3be2f12dd1a441ccd0b8f4f
puakehaulani <puakehaulani@gmail.com>
2023-11-08 09:41:52 -0600

    ch7 practice assignment instructions

---
+++ module_3/ch7/scales_Practice7.1.py
@@ -0,0 +1,14 @@
+# Practice Assignment Ch. 7-1
+# 1. Lottery number generator: Write a program that generates a
+# seven-digit lottery number. The program should have a loop
+# to generate seven random numbers, each in the range 0
+# through 9 and assign each number to a list element.
+# 2. Write another loop to display the contents of the list.
+# 3. Tip: You will need to create/initialize your list before you can
+# assign numbers to it.
+# 4. Use program 7-1 sales_list as an example. You will start with
+# a seven-digit lottery number that contains all zeros. Then in
+# your loop, you will assign a random number instead of
+# getting the data from the user.
+# Turn in your program to the practice assignment link in course
+# content.
--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-for row in range(10): 
-    for col in range(15): 
-        print('#', end='')	 
-    print() 
+import math
+def calc_circle_is_bigger_than_square(radius, length):
+    circle_area = math.pi * radius **2
+    square_area = length **2
+    if circle_area > square_area:
+        circle_bigger = True
+    else:
+        circle_bigger = False
+    return circle_bigger, circle_area, square_area
+
+def main():
+    display = calc_circle_is_bigger_than_square(25, 10)
+    print(display)
+    
+main()
