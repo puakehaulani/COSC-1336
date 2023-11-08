fddf31459875f37db2710335967f70b659954b98
puakehaulani <puakehaulani@gmail.com>
2023-11-08 09:55:15 -0600

    ch7 practice assignment instructions update

---
+++ module_3/ch7/scales_Practice7B.1.py
@@ -0,0 +1,14 @@
+# Practice Assignment Ch. 7-3
+# 1. Write a program that contains a main function and a
+# sub-function called total_list.
+# 2. The main function will create two lists containing five
+# integers (of your choosing) each
+# 3. The main function will join the two lists together, sort
+# the resulting 10 integer list, and pass it to the
+# total_list function
+# 4. The total_list function will total the integers in the
+# list and return the total to the main function
+# 5. The main function will print the list, print the total, and
+# print the maximum and minimum values in the list.
+# Turn in your program to the practice assignment
+# link in course content.
---
+++ module_3/ch7/scales_Practice7B.2.py
@@ -0,0 +1,10 @@
+# Practice Assignment Ch. 7-4
+# Write a program that creates a two dimensional list with 4
+# rows and 3 columns.
+# 1. Write a nested loop to get an integer value from the user
+# for each element in the list.
+# 2. Write another nested loop to sum and display each
+# element in the list.
+# 3. Print the total of all elements
+# Turn in your program to the practice assignment link in
+# course content.
