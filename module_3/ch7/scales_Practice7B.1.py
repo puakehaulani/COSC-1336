2d49051f714042bfeb00691dbbf29a15462760a8
puakehaulani <puakehaulani@gmail.com>
2023-11-08 22:50:14 -0600

    ch7b1 completed

--- module_3/ch7/scales_Practice7B.1.py
+++ module_3/ch7/scales_Practice7B.1.py
@@ -12,3 +12,32 @@
 # print the maximum and minimum values in the list.
 # Turn in your program to the practice assignment
 # link in course content.
+
+def main():
+    # create two lists containing five integers each
+    list = [0, 6, 2, 1, 8]
+    list2 = [6, 8, 1, 2, 1]
+    # join the two lists together
+    list += list2
+    # sort resulting list
+    list.sort()
+    # call total list function, passing it new sorted list
+    total = total_list(list)
+    # print list
+    print(f'Sorted list: {list}')
+    # print total
+    print(f'Total of list: {total:,}')
+    # print max and min values in list
+    print(f'Max: {max(list)}\tMin: {min(list)}')
+
+def total_list(list):
+    # instantiate total var
+    total = 0
+    # total integers in list
+    for num in list:
+        total += num
+    # return total to main function
+    return total
+
+if __name__ == '__main__':
+    main()
