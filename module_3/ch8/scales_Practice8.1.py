63a52e4e3ccd9240f0a96f585cf957b18ed52099
puakehaulani <puakehaulani@gmail.com>
2023-11-16 10:38:27 -0600

    starting practice ch 8

--- module_3/ch8/scales_Practice8.1.py
+++ module_3/ch8/scales_Practice8.1.py
@@ -7,3 +7,27 @@
 # 5. Counts the number of “s” or “S” letters
 # 6. Displays each of the three counts
 # Turn in your program to the practice assignment link in course content.
+
+
+    # main() gets input for first middle and last name from user, concatenates them to a full name string, then counts the number of common letters, displaying the counts back to the user.
+def main():
+    # get user input for first, middle and last name
+    first = str(input("Enter your first name: "))
+    middle = str(input("Enter your middle name: "))
+    last = str(input("Enter your last name: "))
+    # concat inputs into full_name string
+    full_name = first + middle + last
+    # count number of a/A letters in full_name
+    a_count = None
+    # count number of e/E letters in full_name
+    e_count = None
+    # count number of s/S letters in full_name
+    s_count = None
+    # print counts of letters
+    print('Common letter counts in full name')
+    print(f'A:{a_count}')
+    print(f'E:{e_count}')
+    print(f'S:{s_count}')
+
+if __name__ == '__main__':
+    main()
--- module_3/ch8/scales_Practice8.2.py
+++ module_3/ch8/scales_Practice8.2.py
@@ -8,3 +8,19 @@
 # 4. Print the original sentence and the new sentence.
 # 5. Turn in your program to the practice assignment link in course
 # content.
+
+# main() switches the casing of letters in a string. uppercase become lowercase, lowercase become uppercase. other characters are unchanged.
+def main():
+    pass
+    # get user input of a sentence containing uppercase, lowercase, and special char
+    original_sentence = str(input('Input a sentence here. Please include lowercase, uppercase, and special characters.\n'))
+    # create copy of string with case flipped
+    flipped_sentence = None
+    # print original sentence
+    print(f'Original sentence:\n{original_sentence}')
+    # print new sentence
+    print(f'Flipped sentence:\n{flipped_sentence}')
+
+
+if __name__ == '__main__':
+    main()
