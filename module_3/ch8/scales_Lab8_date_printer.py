db9c818b5e018c083aa638a4891e63b84ab043d0
puakehaulani <puakehaulani@gmail.com>
2023-11-18 14:14:02 -0600

    complete lab2

--- module_3/ch8/scales_Lab8_date_printer.py
+++ module_3/ch8/scales_Lab8_date_printer.py
@@ -1,34 +1,40 @@
-# Problem 2.  Date Printer - Write a program that reads a string from the user containing a date in the form mm/dd/yyyy.   It should print the date in the format March 12, 2018.  
-
-# Sample dialog:
-# Enter a date in the format mm/dd/yyyy: 01/16/2018
-# January 16, 2018
-
-# You should have 2 .py files to turn in.   
-
-
-# lexi scales, incomplete.
+# lexi scales, complete.
 # this program reads a string from the user containing a date in the form mm/dd/yyyy, and converts it to be printed in format Month day, year.
 
-import re
 import datetime
+
+month_dict = {
+    "1": "January",
+    "2": "February",
+    "3": "March",
+    "4": "April",
+    "5": "May",
+    "6": "June",
+    "7": "July",
+    "8": "August",
+    "9": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"
+}
 
 def main():
     # enter_date function, with return saved to var
     user_date = enter_date()
-    print(user_date)
-    # format to Month day, year
-    
+    # split string to pieces to reformat
+    date_tokens = user_date.split('/')    
     # print new format
+    print(f'month_dict[date_tokens[0]] {date_tokens[1]}, {date_tokens[2]}')
     
 # enter_date prompts a user to enter a date then verifies it is a valid date in the format specified. it handles exceptions by rerunning the function. it returns the valid date object.
 def enter_date():
     try:
     # user enters date in format mm/dd/yyy
         date_input = str(input('Enter a date in the format mm/dd/yyy: '))
-    # verify entry meets format
+    # verify entry meets format and is a valid date
         datetime.datetime.strptime(date_input, '%m/%d/%Y')
         return date_input
+    # rerun function if encounters exception
     except Exception as err:
         print(f'Error: {err}')
         return enter_date()
