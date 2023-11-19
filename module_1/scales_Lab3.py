bc73f584390ac6755ec0a9ecf7114d2a1e4673bf
puakehaulani <puakehaulani@gmail.com>
2023-11-18 20:56:18 -0600

    added named constants

--- module_1/scales_Lab3.py
+++ module_1/scales_Lab3.py
@@ -4,17 +4,26 @@
 
 BASE_PRICE = 149
 number_purchased = int(input('Enter the number of packages purchased: '))
+bulk_10 = 10
+bulk_50 = 50
+bulk_100 = 100
+bulk_150 = 150
+percent_10 = 0
+percent_50 = .1
+percent_100 = .2
+percent_150 = .3
+percent_max = .4
 
-if number_purchased < 10:
-    discount = 0
-elif number_purchased < 50:
-    discount = .1
-elif number_purchased < 100:
-    discount = .2
-elif number_purchased < 150:
-    discount = .3
+if number_purchased < bulk_10:
+    discount = percent_10
+elif number_purchased < bulk_50:
+    discount = percent_50
+elif number_purchased < bulk_100:
+    discount = percent_100
+elif number_purchased < bulk_150:
+    discount = percent_150
 else:
-    discount = .4
+    discount = percent_max
     
 discount_amount = (number_purchased * BASE_PRICE) * discount
 total_amount = (number_purchased * BASE_PRICE) - discount_amount
