9f8bbe8d1edd274a0721eaa0a53c33f10247512f
puakehaulani <puakehaulani@gmail.com>
2023-11-24 13:50:50 -0600

    dog farm practice

--- module_3/ch9/scales_Practice9.1.py
+++ module_3/ch9/scales_Practice9.1.py
@@ -11,3 +11,114 @@
 #  Person 3 – Add function
 #  Person 4 - Change, and Delete functions
 # Turn in your programs/modules to the practice assignment link in course content.
+
+
+# This program uses a dictionary to keep track of foster dogs
+
+# Global constants for menu choices
+LOOK_UP = 1
+ADD = 2
+CHANGE = 3
+DELETE = 4
+QUIT = 5
+
+DEFAULT = 'Pupper not found in this pack.'
+# main function
+def main():
+    # Create an empty dictionary.
+    foster_dogs = {}
+
+    # Initialize a variable for the user's choice.
+    choice = 0
+
+    while choice != QUIT:
+        # Get the user's menu choice.
+        choice = get_menu_choice()
+
+        # Process the choice.
+        if choice == LOOK_UP:
+            look_up(foster_dogs)
+        elif choice == ADD:
+            add(foster_dogs)
+        elif choice == CHANGE:
+            change(foster_dogs)
+        elif choice == DELETE:
+            delete(foster_dogs)
+
+# The get_menu_choice function displays the menu and gets a validated choice from the user.
+def get_menu_choice():
+    print()
+    print('===========================')
+    print('Foster dogs at the Dog Farm')
+    print('---------------------------')
+    print('1. Look up a dog')
+    print('2. Add a new dog to the pack')
+    print('3. Change a dog\'s decription')
+    print('4. Remove adopted dog from the Farm')
+    print('5. Quit the program')
+    print()
+
+    # Get the user's choice.
+    choice = int(input('Enter your choice: '))
+
+    # Validate the choice.
+    while choice < LOOK_UP or choice > QUIT:
+        choice = int(input('Enter a valid choice: '))
+
+    # return the user's choice.
+    return choice
+
+# The look_up function looks up a name in the foster_dogs dictionary.
+def look_up(foster_dogs):
+    # Get a name to look up.
+    name = input('Enter a dog\'s name: ').lower()
+
+    # Look it up in the dictionary.
+    description = foster_dogs.get(name, DEFAULT)
+    if description is not DEFAULT:
+        print(f'{name.capitalize()}\'s description: {description}')
+    else:
+        print(DEFAULT)
+
+# The add function adds a new entry into the foster_dogs dictionary.
+def add(foster_dogs):
+    # Get a name and description.
+    name = input('Enter the dog\'s name: ').lower()
+    description = input('Enter a word to describe the dog: ')
+
+    # If the name does not exist, add it.
+    if name not in foster_dogs:
+        foster_dogs[name] = description
+    else:
+        print(f'A profile for {name.capitalize()} already exists.\n')
+
+# The change function changes an existing entry in the foster_dogs dictionary.
+def change(foster_dogs):
+    # Get a name to look up.
+    name = input('Enter the dog\'s name: ').lower()
+
+    if name in foster_dogs:
+        # Get a new description.
+        description = input('Enter a new description word: ')
+
+        # Update the entry.
+        foster_dogs[name] = description
+    else:
+        print(f'{DEFAULT}\n')
+        
+# The delete function deletes an entry from the foster_dogs dictionary when a dog is adopted.
+def delete(foster_dogs):
+    # Get a name to look up.
+    name = input('Enter the dog\'s name: ').lower()
+
+    # If the name is found, delete the entry.
+    if name in foster_dogs:
+        del foster_dogs[name]
+        print(f'Congrats on finding {name.capitalize()} their furever home!\n')
+    else:
+        print(F'{DEFAULT}\n')
+
+# Call the main function.
+main()
+
+    
