f6d262039885e6b80d2516da9c967095a1fe3c21
puakehaulani <puakehaulani@gmail.com>
2023-11-26 10:21:17 -0600

    finished stupid shit

--- module_3/ch9/scales_Lab9.py
+++ module_3/ch9/scales_Lab9.py
@@ -1,4 +1,4 @@
-# lexi scales, incomplete
+# lexi scales, complete
 # this program contains dictionaries with class information. a user enters a course number and it will display the course information. a user can continue entering course numbers until they quit the program.
 
 # create dictionary with course numbers and room numbers for the course
@@ -29,8 +29,37 @@ course_time_dict = {
 }
 
 # main
-# loop while quit != true
-    # get user input (course number or quit)
-    # check if course is valid
+def main():
+    while True:
+        # get user choice
+        choice = enter_choice()
+        # convert to lowercase
+        choice_lower = choice.lower()
+        # check if user chose to quit
+        if choice_lower == "q":
+            break
+        # check if course is valid
         # if valid, display course info from dictionaries
+        if choice_lower in course_room_dict:
+            print(f'The details for course {choice} are:')
+            print(f'Room: {course_room_dict[choice]}')
+            print(f'Instructor: {course_instructor_dict[choice]}')
+            print(f'Time: {course_time_dict[choice]}')
         # if not valid, display error message
+        if choice_lower not in course_room_dict:
+            print(f'{choice} is an invalid course number.')
+            
+def enter_choice():
+    try:
+        # get user input
+        return str(input('Enter a course number, or q to stop: '))
+    except ValueError as err:
+        print(f'Error\n{err}')
+        return enter_choice()
+    except Exception as err:
+        print(f'Error\n{err}')
+        return enter_choice()
+        
+
+if __name__ == '__main__':
+    main()
