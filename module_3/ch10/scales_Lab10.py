b2e694bf22b9cb8a04d87159daae476c850bbf14
puakehaulani <puakehaulani@gmail.com>
2023-12-01 19:04:48 -0600

    lab10 final

--- module_3/ch10/scales_Lab10.py
+++ module_3/ch10/scales_Lab10.py
@@ -1,46 +1,5 @@
-# Lab 10 – Classes 
-#  Main program: 
-# Your main program should create three instances of the class.   Your program should get the information from the user and pass it as parameters to the initializer method.    Using the __str__ method invoked by the print function, the program should display the personal information for the three individuals.  
-
-# Output and Sample Dialog: 
-# Enter employee name: Mary Smith 
-# Enter employee ID:  123456 
-# Enter department: Accounting 
-# Enter position: Accountant 
-
-# Enter employee name: Joe Morales 
-# Enter employee ID:  678910 
-# Enter department: Engineering 
-# Enter position: Engineer 
-
-# Enter employee name: Marie Zinc 
-# Enter employee ID:  45678 
-# Enter department: Customer Service 
-# Enter position: Customer Service Rep 
-
-
-# Employee  1 : 
-# Name: Mary Smith 
-# ID number: 123456 
-# Department: Accounting 
-# Title: Accountant 
-
-# Employee  2 : 
-# Name: Joe Morales 
-# ID number: 678910 
-# Department: Engineering 
-# Title: Engineer 
-
-# Employee  3 : 
-# Name: Marie Zinc 
-# ID number: 45678 
-# Department: Customer Service 
-# Title: Customer Service Rep 
-
-# Note:   You may not actually be using the setter and getter methods but to be complete, your class needs to include them.     
-# See Cellphone class example.  Instead of using this method of printing, invoke __str__  in main by using  print(object_name). 
-# Turn in two files, employee.py and your main program (yourlastname_Lab10.py). 
-
+# lexi scales, complete
+# this program creates three instances of the employee class. it returns the information to the user.
 import employee
 
 # Your main program should create three instances of the class.   Your program should get the information from the user and pass it as parameters to the initializer method.    Using the __str__ method invoked by the print function, the program should display the personal information for the three individuals. 
